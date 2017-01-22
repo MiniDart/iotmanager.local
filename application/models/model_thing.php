def777d74f61c4650fc6ca631c637aadeb098c1e
miniDart <lu036@yandex.ru>
2017-01-22 22:33:25 +0300

    Have made a actions_data receiving from device, without time yet

--- application/models/model_thing.php
+++ application/models/model_thing.php
@@ -41,17 +41,60 @@ class Model_thing extends Model
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(array('device_id' => $device_id)));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec ($curl);
-        $currentActionsData=json_decode($server_output);
+        $current_action_data=json_decode($server_output,true);
         $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
         if ($mysqli->connect_errno) {
             die("Не удалось подключиться к MySQL");
         }
-        foreach ($currentActionsData['actions'] as $action){
-            
+        $count_action_group=count($current_action_data['actionGroups']);
+        for ($x=0;$x<$count_action_group;$x++) {
+            $group_name=$current_action_data['actionGroups'][$x]['name'];
+            $query="SELECT id FROM action_groups WHERE thing_id='$current_action_data[thing_id]' AND group_name='$group_name'";
+            if ($res = $mysqli->query($query)) {
+                $assoc_res = $res->fetch_all(MYSQLI_ASSOC);
+                $group_id = $assoc_res[0]['id'];
+                $res->close();
+            } else {
+                echo "Can't get results from database in Model_thing->get_current_data()";
+                $mysqli->close();
+                return false;
+            }
+            $current_action_data['actionGroups'][$x]['id']=$group_id;
+            $count_current_action_data = count($current_action_data['actionGroups'][$x]['actions']);
+            for ($i = 0; $i < $count_current_action_data; $i++) {
+                $name = $current_action_data['actionGroups'][$x]['actions'][$i]['name'];
+                $query = "SELECT id,action_group_id FROM actions_description WHERE action_name='$name' AND action_group_id=$group_id";
+                if ($res = $mysqli->query($query)) {
+                    $assoc_res = $res->fetch_all(MYSQLI_ASSOC);
+                    $action_id = $assoc_res[0]['id'];
+                    $res->close();
+                } else {
+                    echo "Can't get results from database in Model_thing->get_current_data()";
+                    $mysqli->close();
+                    return false;
+                }
+                $current_action_data['actionGroups'][$x]['actions'][$i]['id'] = $action_id;
+                if (isset($current_action_data['actionGroups'][$x]['actions'][$i]['supportActions'])) {
+                    $count_support_action_data = count($current_action_data['actionGroups'][$x]['actions'][$i]['supportActions']);
+                    for ($l = 0; $l < $count_support_action_data; $l++) {
+                        $support_action_name = $current_action_data['actionGroups'][$x]['actions'][$i]['supportActions'][$l]['name'];
+                        $query = "SELECT id FROM support_actions WHERE action_name='$support_action_name' AND action_owner_id=$action_id";
+                        if ($res = $mysqli->query($query)) {
+                            $support_action_id = $res->fetch_all(MYSQLI_ASSOC)[0]['id'];
+                            $res->close();
+                        } else {
+                            echo "Can't get results from database in Model_thing->get_current_data(). Error=" . $mysqli->error;
+                            $mysqli->close();
+                            return false;
+                        }
+                        $current_action_data['actionGroups'][$x]['actions'][$i]['supportActions'][$l]['id'] = $support_action_id;
+                    }
+                }
+            }
         }
-        $query="";
         $mysqli->close();
-        return $server_output;
+        $current_action_data=json_encode($current_action_data);
+        return $current_action_data;
     }
     public function get_upgrade_device_data(){
         $param=json_decode($_POST['newData'],true);
--- application/views/js/thing.js
+++ application/views/js/thing.js
@@ -56,6 +56,7 @@ class Item{
         this.owner=owner;
         if ("color" in data) this.color=data.color;
         else this.color=null;
+        this.id=data.id;
         this.format=owner.format;
             switch (this.format) {
                 case "list":
@@ -146,21 +147,25 @@ class Action{
         if (this.isChangeable){
             let from=null;
             let to=null;
-            if (this.range!=null) {
-                from=this.range[0].from;
-                to=this.range[0].to;
+            if (this.range!=null&&this.format!="list") {
+                let range=[];
+                for (let item of this.range.values()){
+                    range.push(item);
+                }
+                from=range[0].from;
+                to=range[0].to;
             }
             switch (this.format){
                 case "number":
                     actionDom.append("<p>Введите новое значение"+(from||to?" в диапозоне":"")+(from?" от "+from:"")+(to?" до"+to:"")+":</p>");
-                    actionDom.append("<input name='newValue' type='text'>");
+                    actionDom.append("<input class='newValue' type='text'>");
                     break;
                 case "list":
                     actionDom.append("<p>Выберите новое значение:</p>");
-                    let selectDom=$("<select name='newValue'></select>");
+                    let selectDom=$("<select class='newValue'></select>");
                     selectDom.append($("<option value='-1'>none</option>"));
-                    for (let i=0;i<this.range.length;i++){
-                        selectDom.append($("<option value='"+i+"'>"+this.range[i].name+"</option>"));
+                    for (let [key,value] of this.range){
+                        selectDom.append($("<option value='"+key+"'>"+value.name+"</option>"));
                     }
                     actionDom.append(selectDom);
                     break;
@@ -180,9 +185,9 @@ class SupportAction extends Action{
         this.isIndividual = data.isIndividual == "true";
         this.range = null;
         if ("range" in data) {
-            let r = [];
+            let r = new Map();
             for (let i = 0; i < data.range.length; i++) {
-                r.push(new SupportItem(data.range[i], this))
+                r.set(data.range[i].id,new SupportItem(data.range[i], this));
             }
             this.range = r;
         }
@@ -196,9 +201,11 @@ class SupportAction extends Action{
             "class":"supportAction",
             "id":"supportAction_"+this.id
         });
+        supportActionDom.find(".value").addClass("support");
         if (this.isChangeable&&this.isIndividual){
             supportActionDom.append("<div class='submit'><input type='submit' value='"+(this.submitName?this.submitName:"Отправить")+"'></div>")
         }
+        supportActionDom.find(".newValue").addClass("support");
         this.domElement=supportActionDom;
         return supportActionDom;
     }
@@ -209,17 +216,17 @@ class MainAction extends Action{
         super(data,owner);
         this.range=null;
         if ("range" in data){
-            let r=[];
+            let r=new Map();
             for (let i=0;i<data.range.length;i++){
-                r.push(new MainItem(data.range[i],this))
+                r.set(data.range[i].id,new MainItem(data.range[i],this));
             }
             this.range=r;
         }
         this.supportActions=null;
         if ("support" in data){
-            let actions=[];
+            let actions=new Map();
             for (let i=0;i<data.support.length;i++){
-                actions.push(new SupportAction(data.support[i],this));
+                actions.set(data.support[i].id,new SupportAction(data.support[i],this));
             }
             this.supportActions=actions;
         }
@@ -230,27 +237,32 @@ class MainAction extends Action{
             "class":"mainAction",
             "id":"mainAction_"+this.id
         });
+        mainActionDom.find(".value").addClass("main");
+        mainActionDom.find(".newValue").addClass("main");
         mainActionDom.append("<div class='support'></div>");
         if (this.isChangeable){
             mainActionDom.append("<div class='submit'><input type='submit' value='"+(this.submitName?this.submitName:"Отправить")+"'></div>")
         }
         if (this.format=="list") {
             let self=this;
-            mainActionDom.find("[name=newValue]").change(function (event) {
+            mainActionDom.find(".newValue").change(function (event) {
                 let val = $(this).val();
-                let textVal = $(this).find("[value=" + val + "]").text();
+                let textVal=null;
+                if (self.range.has(+val)){
+                    textVal=self.range.get(+val).name;
+                }
                 let supportActions=[];
-                for (let i=0;i<self.supportActions.length;i++){
-                    if (!self.supportActions[i].active){
-                        supportActions.push(self.supportActions[i]);
+                for (let supportAction of self.supportActions.values()){
+                    supportAction.domElement.hide();
+                    if (!supportAction.active){
+                        supportActions.push(supportAction);
                     }
-                    else {
-                        let activeItems=self.supportActions[i].active.split(":");
-                        if (activeItems.indexOf(textVal)!=-1) supportActions.push(self.supportActions[i]);
+                    else if (textVal!=null){
+                        let activeItems=supportAction.active.split(":");
+                        if (activeItems.indexOf(textVal)!=-1) supportActions.push(supportAction);
                     }
                 }
                 let algorithm=drawManager.activeTheme.algorithm;
-                mainActionDom.find(".support").empty();
                 drawManager[algorithm+"SupportAlgorithm"](supportActions,mainActionDom,mainActionDom.width());
             });
         }
@@ -267,10 +279,10 @@ class ActionGroup{
         this.id=data.id;
         this.rank=data.rank;
         let actions=[];
+        this.actions=new Map();
         for (let i=0;i<data.actions.length;i++){
-            actions.push(new MainAction(data.actions[i],this));
+            this.actions.set(data.actions[i].id,new MainAction(data.actions[i],this));
         }
-        this.actions=actions;
         this.domElement=null;
     }
     draw(){
@@ -291,11 +303,10 @@ class Device{
         this.name=makeBigFirstLetter(data.name);
         this.group=data.thingGroup;
         this.updateTime=data.updateTime;
-        let actGroups=[];
+        this.actionGroups=new Map();
         for (let i=0;i<data.actionGroups.length;i++){
-            actGroups.push(new ActionGroup(data.actionGroups[i],this));
+            this.actionGroups.set(data.actionGroups[i].id,new ActionGroup(data.actionGroups[i],this));
         }
-        this.actionGroups=actGroups;
         this.domElement=null;
     }
     draw(){
@@ -310,8 +321,25 @@ class Device{
         let self=this;
         $.post("getdata",{device_id:self.id},onAjaxSuccess,'json');
         function onAjaxSuccess(data) {
-            console.log(data);
-            console.dir(data);
+            if (data.thing_id==self.id){
+                let dataActionGroups=data.actionGroups;
+                for (let i=0;i<dataActionGroups.length;i++){
+                    let actionGroup=self.actionGroups.get(+dataActionGroups[i].id);
+                    let dataActions=dataActionGroups[i].actions;
+                    for (let l=0;l<dataActions.length;l++){
+                        let action=actionGroup.actions.get(+dataActions[l].id);
+                        action.domElement.find(".value").filter(".main").text(dataActions[l].value);
+                        if ("supportActions" in dataActions[l]){
+                            let dataSupportActions=dataActions[l].supportActions;
+                            for (let n=0;n<dataSupportActions.length;n++){
+                                let supportAction=action.supportActions.get(+dataSupportActions[n].id);
+                                supportAction.domElement.find(".value").text(dataSupportActions[n].value);
+                            }
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
@@ -340,16 +368,6 @@ class DrawManager{
     }
     draw(){
         $("h1.device_name").text(this.device.name);
-        let groups=this.device.actionGroups;
-        groups.sort(this.rankSort);
-        for (let i=0;i<groups.length;i++){
-            groups[i].actions.sort(this.rankSort);
-            for (let l=0;l<groups[i].actions.length;l++){
-                if (groups[i].actions[l].supportActions!=null) {
-                    groups[i].actions[l].supportActions.sort(this.rankSort);
-                }
-            }
-        }
         this[this.activeTheme.algorithm+"Algorithm"]();
     }
     simpleAlgorithm(){
@@ -365,8 +383,11 @@ class DrawManager{
             "color":"white",
             "font-size":"20px"
         });
-        let actionGroups=this.device.actionGroups;
-
+        let actionGroups=[];
+        for (let group of this.device.actionGroups.values()){
+            actionGroups.push(group);
+        }
+        actionGroups.sort(this.rankSort);
         for (let i=0;i<actionGroups.length;i++){
             let actionGroupDom=actionGroups[i].draw();
             actionGroupDom.find("h2").css("marginBottom","30px");
@@ -380,7 +401,11 @@ class DrawManager{
             if (actionGroups.length!=1){
                 actionGroupDom.css("border","1px solid black");
             }
-            let actions=actionGroups[i].actions;
+            let actions=[];
+            for (let action of actionGroups[i].actions.values()){
+                actions.push(action);
+            }
+            actions.sort(this.rankSort);
             let actionWidth=Math.floor(containerWidth/this.activeTheme.allLines)-this.activeTheme.allLines*12;
             for (let l=0;l<actions.length;l++){
                 let actionDom=actions[l].draw();
@@ -392,13 +417,33 @@ class DrawManager{
                 });
                 actionDom.find("div.value").css({
                     "height":"30px",
-                    "background":"grey"
+                    "background":"grey",
+                    "text-align":"center"
                 });
                 let supportActions=actions[l].supportActions;
                 if (supportActions!=null) {
+                    let supportContainerDom=actionDom.find("div.support");
+                    supportContainerDom.css({
+                        "display":"flex",
+                        "flexWrap":"wrap",
+                        "justifyContent":"space-around",
+                        "padding":"5px"
+                    });
                    let activeSupportActions=[];
-                    for (let m=0;m<supportActions.length;m++){
-                        if (supportActions[m].active==null) activeSupportActions.push(supportActions[m]);
+                    for (let supportAction of supportActions.values()){
+                        let supportActionDom=supportAction.draw();
+                        supportActionDom.hide();
+                        supportActionDom.css({
+                            "border": "1px solid grey",
+                            "padding":"5px"
+                        });
+                        supportActionDom.find("div.value").css({
+                            "height": "30px",
+                            "background": "grey",
+                            "text-align":"center"
+                        });
+                        supportContainerDom.append(supportActionDom);
+                        if (supportAction.active==null) activeSupportActions.push(supportAction);
                     }
                     this.simpleSupportAlgorithm(activeSupportActions,actionDom,actionWidth);
                 }
@@ -412,12 +457,6 @@ class DrawManager{
     }
     simpleSupportAlgorithm(supportActions,actionDom,actionWidth){
         let supportContainerDom=actionDom.find("div.support");
-        supportContainerDom.css({
-            "display":"flex",
-            "flexWrap":"wrap",
-            "justifyContent":"space-around",
-            "padding":"5px"
-        });
         let supportActionWidth=Math.floor(actionWidth/supportActions.length);
         if (supportActionWidth<250) {
             let w = Math.floor(actionWidth / 250);
@@ -425,17 +464,11 @@ class DrawManager{
         }
         else supportActionWidth-=supportActions.length*12;
         for (let m = 0; m < supportActions.length; m++) {
-            let supportActionDom = supportActions[m].draw();
+            let supportActionDom = supportActions[m].domElement;
             supportActionDom.css({
                 "width": supportActionWidth + "px",
-                "border": "1px solid grey",
-                "padding":"5px"
             });
-            supportActionDom.find("div.value").css({
-                "height": "30px",
-                "background": "grey"
-            });
-            supportContainerDom.append(supportActionDom);
+            supportActionDom.show();
         }
     }
     rankSort(a,b){
